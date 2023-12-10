fa7f428bb518fc1cbfd9ed9220fcdd0d9e1f613c
Miguel <whytemig@gmail.com>
2023-12-09 23:17:26 -0500

    updates

--- maximum_subarray.js
+++ maximum_subarray.js
@@ -2,3 +2,23 @@
 //  with the largest sum, and return its sum.
 
 // [1,-2,3,7,-4,-5,6,-10,]
+
+function theMax(nums) {
+  let maxNum = nums[0];
+  let sum = 0;
+  for (let i = 1; i < nums.length; i++) {
+    sum += nums[i];
+
+    if (sum >= maxNum) {
+      maxNum = sum;
+    } else if (sum < 0) {
+      sum = 0;
+    }
+
+    maxNum = Math.max(maxNum, sum);
+  }
+
+  return maxNum;
+}
+
+console.log(theMax([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
