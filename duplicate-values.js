886f9216e225793b8bfb13fde7cd74d197bfb7b9
Miguel <whytemig@gmail.com>
2023-12-12 19:01:23 -0500

    new equation

--- duplicate-values.js
+++ duplicate-values.js
@@ -1,9 +1,21 @@
-var containsDuplicate = function (nums) {
-  nums.sort((a, b) => a - b);
+// var containsDuplicate = function (nums) {
+//   nums.sort((a, b) => a - b);
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === nums[i + 1]) return true;
+//   }
+//   return false;
+// };
+
+function containsDuplicate(nums) {
+  let unique = new Set();
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === nums[i + 1]) return true;
+    if (unique.has(nums[i])) {
+      return true;
+    }
+    unique.add(nums[i]);
   }
+
   return false;
-};
+}
 
 console.log(containsDuplicate([1, 2, 4, 8, 9, 2, 4, 22, 3]));
